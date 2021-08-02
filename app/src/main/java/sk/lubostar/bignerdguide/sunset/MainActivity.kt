4e676d2809ea0c2916b79e53eb9c0ac4ec64941c
staracek <staracek@foundation.sk>
2021-08-02 17:22:40 +0200

    initial implementation of the layout and the animation

--- app/src/main/java/sk/lubostar/bignerdguide/sunset/MainActivity.kt
+++ app/src/main/java/sk/lubostar/bignerdguide/sunset/MainActivity.kt
@@ -1,11 +1,29 @@
 package sk.lubostar.bignerdguide.sunset
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        scene.setOnClickListener {
+            startAnimation()
+        }
+    }
+
+    private fun startAnimation() {
+        val sunYStart = sun.top.toFloat()
+        val sunYEnd = sky.height.toFloat()
+
+        val heightAnimator = ObjectAnimator
+                .ofFloat(sun, "y", sunYStart, sunYEnd)
+                .setDuration(3000)
+
+        heightAnimator.start()
     }
 }
